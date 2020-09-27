56fc7f13e506ff54edcf9833c59b3ba3760cbacf
pabloMarinoArg <31575175+pabloMarinoArg@users.noreply.github.com>
2020-09-27 18:28:02 -0300

    Add files via upload

---
+++ build/precache-manifest.1c3d81f173bc7992620bad908794fac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b9403a1fa7f0741e590ac9ed040cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8d4a89db106a6460653",
+    "url": "/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "7eeacf68d3aac0144119",
+    "url": "/static/js/2.6a27e046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a27e046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d4a89db106a6460653",
+    "url": "/static/js/main.42cdbd7b.chunk.js"
+  },
+  {
+    "revision": "ff7203c7642c512f520e",
+    "url": "/static/js/runtime-main.21fe8694.js"
+  }
+]);
